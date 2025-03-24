eb8049f355c2cd7cd0c497093398cd893891f334
Prashant Kumar <99476881+prashantkkumar@users.noreply.github.com>
2025-03-24 14:29:35 +0530

    Delete collections/java_generics directory

--- collections/java_generics/dynamic_online_marketplace/ProductCategory.java
+++
@@ -1,5 +0,0 @@
-package java_generics.dynamic_online_marketplace;
-
-public enum ProductCategory {
-    BOOK, CLOTHING, GADGET
-}
--- collections/java_generics/meal_plan_generator/MealPlan.java
+++
@@ -1,7 +0,0 @@
-package java_generics.meal_plan_generator;
-
-public interface MealPlan {
-    String getMealName();
-    int getCalories();
-    void display();
-}
