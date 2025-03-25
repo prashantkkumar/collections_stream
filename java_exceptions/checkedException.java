8f34a659bd3a19e5b8effd850dec07c1d4983274
Prashant <prashantoff61@gmail.com>
2025-03-25 15:28:33 +0530

    [Prashant] add.Checked Exception(Compile Time)

--- java_exceptions/checkedException.java
+++ java_exceptions/checkedException.java
@@ -1,7 +1,23 @@
 package java_exceptions;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 
 public class checkedException {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String fileName = "xyz.txt";//Give the proper file path for the execution
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
     }
 }
+
---
+++ java_exceptions/finallyblockexecution.java
@@ -0,0 +1,4 @@
+package java_exceptions;
+
+public class finallyblockexecution {
+}
